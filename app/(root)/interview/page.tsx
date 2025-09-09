d4771938c84dd3e900bc5e0f18019afdfc6d5be9
WillowsCosmic <purbalitech8@gmail.com>
2025-09-09 08:36:55 +0530

    new commit

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,12 +1,15 @@
 import Agent from '@/components/Agent'
+import { getCurentUser } from '@/lib/actions/auth.action'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+  const user = await getCurentUser();
+
   return (
     <>
         <h3>Interview Generation</h3>
 
-        <Agent userName="You" userId="user1" type="generate" />
+        <Agent userName={user?.name} userId={user?.id} type="generate" />
     </>
   )
 }
